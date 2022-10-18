81ce243f058eee1efeb3e8084da2a784b5ed59ae
Harrison Chow <harrisonmchow@gmail.com>
2022-10-18 20:37:42 +1100

    cleaned up code

--- src/main/java/dungeonmania/entities/enemies/Mercenary.java
+++ src/main/java/dungeonmania/entities/enemies/Mercenary.java
@@ -1,9 +1,5 @@
 package dungeonmania.entities.enemies;
 
-import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
-
 import dungeonmania.Game;
 import dungeonmania.entities.Entity;
 import dungeonmania.entities.Interactable;
@@ -68,32 +64,6 @@ public class Mercenary extends Enemy implements Interactable {
         super.changeMovement(new RandomMovement(this));
         bribe(player);
     }
-
-    // @Override
-    // public void move(Game game) {
-    // Position nextPos;
-    // GameMap map = game.getMap();
-    // if (allied) {
-    // // Move random
-    // Random randGen = new Random();
-    // List<Position> pos = getPosition().getCardinallyAdjacentPositions();
-    // pos = pos
-    // .stream()
-    // .filter(p -> map.canMoveTo(this, p)).collect(Collectors.toList());
-    // if (pos.size() == 0) {
-    // nextPos = getPosition();
-    // map.moveTo(this, nextPos);
-    // } else {
-    // nextPos = pos.get(randGen.nextInt(pos.size()));
-    // map.moveTo(this, nextPos);
-    // }
-    // } else {
-    // // Follow hostile
-    // nextPos = map.dijkstraPathFind(getPosition(), map.getPlayer().getPosition(),
-    // this);
-    // }
-    // map.moveTo(this, nextPos);
-    // }
 
     @Override
     public boolean isInteractable(Player player) {
--- src/main/java/dungeonmania/entities/enemies/Spider.java
+++ src/main/java/dungeonmania/entities/enemies/Spider.java
@@ -1,19 +1,10 @@
 package dungeonmania.entities.enemies;
 
-import java.util.List;
-
-import dungeonmania.Game;
-import dungeonmania.entities.Boulder;
 import dungeonmania.entities.Entity;
-import dungeonmania.entities.enemies.MovementBehaviour.Movement;
 import dungeonmania.entities.enemies.MovementBehaviour.SpiderMovement;
 import dungeonmania.util.Position;
 
 public class Spider extends Enemy {
-
-    private List<Position> movementTrajectory;
-    private int nextPositionElement;
-    private boolean forward;
 
     public static final int DEFAULT_SPAWN_RATE = 0;
     public static final double DEFAULT_ATTACK = 5;
@@ -27,46 +18,7 @@ public class Spider extends Enemy {
          * 7 S 3 11 S 3/7
          * 6 5 4 B 5 4/6
          */
-        movementTrajectory = position.getAdjacentPositions();
-        nextPositionElement = 1;
-        forward = true;
         super.changeMovement(new SpiderMovement(this, position));
     };
 
-    // private void updateNextPosition() {
-    // if (forward) {
-    // nextPositionElement++;
-    // if (nextPositionElement == 8) {
-    // nextPositionElement = 0;
-    // }
-    // } else {
-    // nextPositionElement--;
-    // if (nextPositionElement == -1) {
-    // nextPositionElement = 7;
-    // }
-    // }
-    // }
-
-    // @Override
-    // public void move(Game game) {
-    // // Position nextPos = movementTrajectory.get(nextPositionElement);
-    // // List<Entity> entities = game.getMap().getEntities(nextPos);
-    // // if (entities != null && entities.size() > 0 &&
-    // entities.stream().anyMatch(e
-    // // -> e instanceof Boulder)) {
-    // // forward = !forward;
-    // // updateNextPosition();
-    // // updateNextPosition();
-    // // }
-    // // nextPos = movementTrajectory.get(nextPositionElement);
-    // // entities = game.getMap().getEntities(nextPos);
-    // // if (entities == null
-    // // || entities.size() == 0
-    // // || entities.stream().allMatch(e -> e.canMoveOnto(game.getMap(), this))) {
-    // // game.getMap().moveTo(this, nextPos);
-    // // updateNextPosition();
-    // // }
-    // Movement movement = super.getMovement();
-    // movement.move(game);
-    // }
 }
