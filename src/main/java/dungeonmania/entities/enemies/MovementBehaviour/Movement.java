5144379088a8b41db6cfc815fe0c2ee3ffedd086
Webster Zhang <webster.zhang@unsw.edu.au>
2022-11-01 13:09:36 +0800

    Assignment II Automarking Dryrun Results

--- src/main/java/dungeonmania/entities/enemies/MovementBehaviour/Movement.java
+++
@@ -1,8 +0,0 @@
-package dungeonmania.entities.enemies.MovementBehaviour;
-
-import dungeonmania.Game;
-import dungeonmania.map.GameMap;
-
-public interface Movement {
-    public void move(Game game, GameMap map);
-}
