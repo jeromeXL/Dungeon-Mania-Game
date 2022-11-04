5f401877e4fb9937c6a91115cdbc62f94eacbb5e
jeromeXL <jerome.chiu99@gmail.com>
2022-11-04 22:38:16 +1100

    Fixing Pylint

--- src/main/java/dungeonmania/entities/logicals/LogicalXOR.java
+++ src/main/java/dungeonmania/entities/logicals/LogicalXOR.java
@@ -15,8 +15,8 @@ public class LogicalXOR implements LogicalStrategy {
     @Override
     public boolean isActivated(GameMap map) {
         // All logical Entities
-        List<LogicalEntity> LogicalEntities = map.getEntities(LogicalEntity.class);
-        int count = (int) LogicalEntities.stream()
+        List<LogicalEntity> logicalEntities = map.getEntities(LogicalEntity.class);
+        int count = (int) logicalEntities.stream()
                 .filter(l -> l != e && Position.isAdjacent(e.getPosition(), l.getPosition()) && l.isActivated(map)
                         && l instanceof Conductor)
                 .count();
