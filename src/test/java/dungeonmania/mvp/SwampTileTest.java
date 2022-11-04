e9f45fc41dd44a5478239585cd50536045a54d54
Harrison Chow <harrisonmchow@gmail.com>
2022-11-04 15:44:59 +1100

    testing

--- src/test/java/dungeonmania/mvp/SwampTileTest.java
+++ src/test/java/dungeonmania/mvp/SwampTileTest.java
@@ -9,7 +9,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class SwampTileTest {
     @Test
     @Tag("21-1")
@@ -17,12 +21,12 @@ public class SwampTileTest {
     public void testPlayerUnaffectedBySwamp() {
         DungeonManiaController dmc = new DungeonManiaController();
         DungeonResponse initDungonRes = dmc.newGame(
-            "d_SwampTileTest_testPlayerUnaffected", "c_SwampTileTest_testPlayerUnaffected");
+                "d_SwampTileTest_testPlayerUnaffected", "c_SwampTileTest_testPlayerUnaffected");
         EntityResponse initPlayer = TestUtils.getPlayer(initDungonRes).get();
 
         // create the expected result
         EntityResponse expectedPlayer = new EntityResponse(initPlayer.getId(),
-        initPlayer.getType(), new Position(2, 1), false);
+                initPlayer.getType(), new Position(2, 1), false);
 
         // move player right
         DungeonResponse actualDungonRes = dmc.tick(Direction.RIGHT);
@@ -30,5 +34,65 @@ public class SwampTileTest {
 
         // assert after movement
         assertTrue(TestUtils.entityResponsesEqual(expectedPlayer, actualPlayer));
+    }
+
+    @Test
+    @Tag("21-2")
+    @DisplayName("Test an enemy mercenary is affected")
+    public void EnemyAffected() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame(
+                "d_SwampTileTest_enemyAffected", "c_SwampTileTest_testPlayerUnaffected");
+
+        res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
+        // The mercenary gets moves onto the swamp tile
+        assertEquals(TestUtils.getEntities(res, "mercenary").get(0).getPosition(),
+                TestUtils.getEntities(res, "swamp_tile").get(0).getPosition());
+        // Should be stuck on the swamp tile for the next 2 turns
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals(TestUtils.getEntities(res, "mercenary").get(0).getPosition(),
+                TestUtils.getEntities(res, "swamp_tile").get(0).getPosition());
+
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals(TestUtils.getEntities(res, "mercenary").get(0).getPosition(),
+                TestUtils.getEntities(res, "swamp_tile").get(0).getPosition());
+
+        // Can move off the tile
+        res = dmc.tick(Direction.RIGHT);
+        assertNotEquals(TestUtils.getEntities(res, "mercenary").get(0).getPosition(),
+                TestUtils.getEntities(res, "swamp_tile").get(0).getPosition());
+    }
+
+    @Test
+    @Tag("21-2")
+    @DisplayName("Test an enemy mercenary is affected")
+    public void AllyAffected() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame(
+                "d_SwampTileTest_enemyAffected", "c_SwampTileTest_testPlayerUnaffected");
+
+        String mercId = TestUtils.getEntitiesStream(res, "mercenary").findFirst().get().getId();
+        // Pick up treasure
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals(1, TestUtils.getInventory(res, "treasure").size());
+        // bribe mercenary
+        assertDoesNotThrow(() -> dmc.interact(mercId));
+        // The allied mercenary gets moves onto the swamp tile
+        assertEquals(TestUtils.getEntities(res, "mercenary").get(0).getPosition(),
+                TestUtils.getEntities(res, "swamp_tile").get(0).getPosition());
+        // Should be stuck on the swamp tile for the next 2 turns
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals(TestUtils.getEntities(res, "mercenary").get(0).getPosition(),
+                TestUtils.getEntities(res, "swamp_tile").get(0).getPosition());
+
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals(TestUtils.getEntities(res, "mercenary").get(0).getPosition(),
+                TestUtils.getEntities(res, "swamp_tile").get(0).getPosition());
+
+        // Can move off the tile
+        res = dmc.tick(Direction.RIGHT);
+        assertNotEquals(TestUtils.getEntities(res, "mercenary").get(0).getPosition(),
+                TestUtils.getEntities(res, "swamp_tile").get(0).getPosition());
     }
 }
